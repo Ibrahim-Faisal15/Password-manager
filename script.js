04f3e6dce94e1157b652aaade333c0c2b467b667
Ibrahim Faisal <hyibrahimfaisal@gmail.com>
2023-12-23 22:34:25 +0500

    added delete function

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let username = document.querySelector("#username");
 let password = document.querySelector("#password");
 let website = document.querySelector("#website");
 let submit = document.querySelector("#submit");
-let delete_row = document.createElement("img");
 
 let password_history = document.querySelector("tbody");
 let table_row = document.createElement("tr");
@@ -18,13 +17,13 @@ password_data.addEventListener("click", (event) => {
 
 username_data.addEventListener("click", (event) => {
   navigator.clipboard.writeText(event.target.textContent);
-  // alert("Copied the text: " + event.target.textContent);
+  alert("Copied the text: " + event.target.textContent);
   console.log(2224);
 });
 
 website_data.addEventListener("click", (event) => {
   navigator.clipboard.writeText(event.target.textContent);
-  // alert("Copied the text: " + event.target.textContent);
+  alert("Copied the text: " + event.target.textContent);
   console.log(2224);
 });
 
@@ -64,6 +63,7 @@ submit.addEventListener("click", (event) => {
   // let username_data = document.createElement("td");
   // let password_data = document.createElement("td");
   // let website_data = document.createElement("td");
+  let delete_row = document.createElement("img");
   delete_row.src = "trash.svg";
   delete_row.style.aspectRatio = 1 / 1;
   delete_row.style.height = "20px";
@@ -71,6 +71,27 @@ submit.addEventListener("click", (event) => {
   delete_row.style.left = "18px";
   delete_row.style.top = "5px";
   delete_row.style.cursor = "pointer";
+
+  delete_row.addEventListener("click", (event) => {
+    // Access the parent row and remove it from the table
+    let rowToDelete = event.target.closest("tr");
+    rowToDelete.remove();
+
+    // Access the stored data in local storage
+    let pass_data = JSON.parse(localStorage.getItem("pass_data"));
+
+    // Identify the index of the item to be deleted
+    let indexToDelete = Array.from(rowToDelete.parentElement.children).indexOf(
+      rowToDelete
+    );
+
+    // Remove the corresponding item from the local storage array
+    pass_data.splice(indexToDelete, 1);
+
+    // Update the local storage with the modified array
+    localStorage.setItem("pass_data", JSON.stringify(pass_data));
+    console.log("Hello Guys");
+  });
 
   username_data.style.cursor = "pointer";
   password_data.style.cursor = "pointer";
@@ -105,6 +126,7 @@ window.addEventListener("load", (event) => {
       let password_data = document.createElement("td");
       let website_data = document.createElement("td");
       let delete_row = document.createElement("img");
+
       delete_row.src = "trash.svg";
       delete_row.style.aspectRatio = 1 / 1;
       delete_row.style.height = "20px";
@@ -120,6 +142,10 @@ window.addEventListener("load", (event) => {
       username_data.textContent = item.username;
       password_data.textContent = item.password;
       website_data.textContent = item.website;
+
+    delete_row.addEventListener("click", function () {
+      this.parentNode.parentNode.removeChild(this.parentNode);
+    });
 
       password_history.appendChild(table_row);
       table_row.appendChild(username_data);
